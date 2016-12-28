80f5ce2d4b69edb52ca678050626de302bae88be
zhangzimeng <zhangzimeng01@baidu.com>
2016-12-28 16:46:58 +0800

    add demo

---
+++ Examples/HelloWorld/js/index.android.js
@@ -0,0 +1,15 @@
+import {
+  AppRegistry
+} from 'react-native';
+
+var UIManager = require('react-native/lib/UIManager');
+
+
+
+AppRegistry.registerComponent('HelloWorld', (initialProps: Props, rootTag: any) => {
+  UIManager.createView(2, "RCTText", rootTag, {height: 100, backgroundColor: -65536});
+  UIManager.createView(3, "RCTRawText", rootTag, {"text":"HelloWorld"});
+  UIManager.setChildren(2, [3]);
+  UIManager.setChildren(1, [2]);
+  alert("123");
+});
